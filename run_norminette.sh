3b4b8da28e78ace14128ebf3f78cf36c59760750
momeyer <meyer.monique93@gmail.com>
2021-05-27 10:46:50 +0200

    first commit

---
+++ run_norminette.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SUCCESS_COLOR="\033[01;32m"
+ERROR_COLOR="\033[01;31m"
+NORMAL_COLOR="\033[0m"
+
+norminette > norminette_result
+
+NORM_ERROR=$(grep -c 'Error!' norminette_result)
+
+if [[ "$NORM_ERROR" > 0 ]]; then
+    printf "$ERROR_COLOR NORM ERROR :(\n"
+    printf "$NORMAL_COLOR incorrect files: [$ERROR_COLOR $NORM_ERROR $NORMAL_COLOR]\n"
+else
+    printf "$SUCCESS_COLOR NORM IS OK :)\n"
+fi
+
+exit $NORM_ERROR
